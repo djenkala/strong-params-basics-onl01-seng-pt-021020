c264e3cf553a768b910981fdb089f2d37263bd7f
Daniel Jenkala <djenkala@yahoo.com>
2020-06-19 16:55:03 +0000

    Automatically backed up by Learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -19,9 +19,9 @@ end
 
 	def update
 	  @post = Post.find(params[:id])
-	  @post.update(params["post"])
-	  redirect_to post_path(@post)
-	end
+  @post.update(params.require(:post).permit(:title))
+  redirect_to post_path(@post)
+end
 
 	def edit
 	  @post = Post.find(params[:id])
